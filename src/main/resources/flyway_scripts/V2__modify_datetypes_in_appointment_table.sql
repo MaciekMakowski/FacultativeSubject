38caec0198b605e7db309549f33c4ff68c54e269
krychuu01 <hazzidy125@gmail.com>
2023-04-25 15:38:11 +0200

    added script to alter date types in appointment table

---
+++ src/main/resources/flyway_scripts/V2__modify_datetypes_in_appointment_table.sql
@@ -0,0 +1,3 @@
+alter table appointments alter column date type varchar(16);
+alter table appointments alter column created_at type varchar(16);
+alter table appointments alter column modified_at type varchar(16);
