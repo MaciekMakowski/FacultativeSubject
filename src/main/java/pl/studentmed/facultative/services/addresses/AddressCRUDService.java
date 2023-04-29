bcb36912e7c59f164ff40e2cedac7a9301fe9e0f
krychuu01 <hazzidy125@gmail.com>
2023-04-29 18:07:58 +0200

    added AddressCreator, AddressCRUDService classes

---
+++ src/main/java/pl/studentmed/facultative/services/addresses/AddressCRUDService.java
@@ -0,0 +1,17 @@
+package pl.studentmed.facultative.services.addresses;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import pl.studentmed.facultative.models.addresses.Address;
+
+@Component
+@RequiredArgsConstructor
+public class AddressCRUDService {
+
+    private final AddressCreator creator;
+
+    public Address createEmptyAddress() {
+        return creator.createEmptyAddress();
+    }
+
+}
---
+++ src/main/java/pl/studentmed/facultative/services/addresses/AddressCreator.java
@@ -0,0 +1,17 @@
+package pl.studentmed.facultative.services.addresses;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import pl.studentmed.facultative.models.addresses.Address;
+
+@Component
+@RequiredArgsConstructor
+class AddressCreator {
+
+    private final AddressRepository repository;
+
+    public Address createEmptyAddress() {
+        var emptyAddress = new Address();
+        return repository.saveAndFlush(emptyAddress);
+    }
+}
