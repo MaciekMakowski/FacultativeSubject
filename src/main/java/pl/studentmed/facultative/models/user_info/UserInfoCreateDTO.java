70fedd294e698ea7d21bdea382d9b852264e538e
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-05-06 15:50:46 +0200

    Added NAME_REGEX to firstName and lastName fields, modified messages with proper modal verb

--- src/main/java/pl/studentmed/facultative/models/user_info/UserInfoCreateDTO.java
+++ src/main/java/pl/studentmed/facultative/models/user_info/UserInfoCreateDTO.java
@@ -7,29 +7,34 @@ import pl.studentmed.facultative.models.validators.role.Role;
 
 import java.time.LocalDate;
 
+import static pl.studentmed.facultative.models.validators.ValueObject.NAME_REGEX;
+
 @Validated
 public record UserInfoCreateDTO(@NotNull
+                                @Pattern(regexp = NAME_REGEX, message = "First name must contain only letters.")
                                 @Size(min = 2, max = 30)
                                 String firstName,
                                 @NotNull
+                                @Pattern(regexp = NAME_REGEX, message = "Last name must contain only letters.")
                                 @Size(min = 2, max = 30)
                                 String lastName,
                                 @NotNull
                                 @Size(max = 30)
-                                @Email String email,
+                                @Email
+                                String email,
                                 @NotNull
                                 @Size(min = 8, max = 25)
                                 String password,
                                 @NotNull
-                                @Pattern(regexp = "\\d+", message = "Phone number must contains only digits.")
+                                @Pattern(regexp = "\\d+", message = "Phone number must contain only digits.")
                                 @Size(min = 9, max = 9)
                                 String phoneNumber,
                                 @NotNull
                                 @Past
                                 LocalDate birthdate,
                                 @NotNull
-                                @Pattern(regexp = "\\d+", message = "Pesel must contains only digits.")
-                                @Size(min = 11, max = 11, message = "Pesel must be 11 chars length.")
+                                @Pattern(regexp = "\\d+", message = "Pesel must contain only digits.")
+                                @Size(min = 11, max = 11, message = "Pesel must be 11 characters length.")
                                 String pesel,
                                 @NotEmpty
                                 @Role
