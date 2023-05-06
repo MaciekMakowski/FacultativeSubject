fbc7f3a54f9de540c48ce2f53e9a5c2cdd102d4d
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-05-06 19:18:29 +0200

    Updated user repo with new methods

--- src/main/java/pl/studentmed/facultative/services/user_info/UserInfoRepository.java
+++ src/main/java/pl/studentmed/facultative/services/user_info/UserInfoRepository.java
@@ -2,9 +2,13 @@ package pl.studentmed.facultative.services.user_info;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.studentmed.facultative.models.user_info.UserInfo;
+import java.util.Optional;
 
 interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
 
-    boolean existsByEmailOrPesel(String email, String pesel);
+    Boolean existsByEmailOrPesel(String email, String pesel);
+    Boolean existsByEmail(String email);
+    Optional<UserInfo> findByEmail(String email);
+    UserInfo findRegisteredUserInfoByEmail(String email);
 
 }
