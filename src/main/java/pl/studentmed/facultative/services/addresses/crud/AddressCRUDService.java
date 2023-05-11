808c8a05469bc40c054c14673ef5940333c6ff88
Adam <trentowski.adam@gmail.com>
2023-05-11 12:09:44 +0200

    Added method to update address in AddressCRUDService

--- src/main/java/pl/studentmed/facultative/services/addresses/crud/AddressCRUDService.java
+++ src/main/java/pl/studentmed/facultative/services/addresses/crud/AddressCRUDService.java
@@ -3,15 +3,28 @@ package pl.studentmed.facultative.services.addresses.crud;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import pl.studentmed.facultative.models.address.Address;
+import pl.studentmed.facultative.models.address.AddressUpdateDTO;
 
 @Component
 @RequiredArgsConstructor
 public class AddressCRUDService {
 
     private final AddressCreator creator;
+    private final AddressReader reader;
+    private final AddressUpdater updater;
 
     public Address createEmptyAddress() {
         return creator.createEmptyAddress();
+    }
+
+    public Address updateAddress(AddressUpdateDTO addressUpdateDTO) {
+        var address = reader.getAddressById(addressUpdateDTO.addressId());
+        return updater.updateAddress(
+                address,
+                addressUpdateDTO.city(),
+                addressUpdateDTO.zipCode(),
+                addressUpdateDTO.street()
+        );
     }
 
 }
