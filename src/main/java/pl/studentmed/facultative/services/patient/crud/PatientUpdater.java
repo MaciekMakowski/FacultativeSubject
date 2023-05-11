aa1c1401ef15ee78e04d9f0093954fdc4dedd7f1
Adam <trentowski.adam@gmail.com>
2023-05-11 12:10:24 +0200

    Added PatientUpdater

--- src/main/java/pl/studentmed/facultative/services/patient/crud/PatientUpdater.java
+++ src/main/java/pl/studentmed/facultative/services/patient/crud/PatientUpdater.java
@@ -2,11 +2,22 @@ package pl.studentmed.facultative.services.patient.crud;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import pl.studentmed.facultative.models.patient.Patient;
 
 @Component
 @RequiredArgsConstructor
 class PatientUpdater {
 
     private final PatientRepository repository;
+
+    public Patient updatePatient(Patient patient, String allergies, String medicines) {
+        if (allergies != null) {
+            patient.setAllergies(allergies);
+        }
+        if (medicines != null) {
+            patient.setMedicines(medicines);
+        }
+        return repository.saveAndFlush(patient);
+    }
 
 }
