3426ab2efe8fddaadd3490d02f5d8dd0a651f323
Adam <trentowski.adam@gmail.com>
2023-05-11 12:10:05 +0200

    Added method to update patient in PatientCRUDService

--- src/main/java/pl/studentmed/facultative/services/patient/crud/PatientCRUDService.java
+++ src/main/java/pl/studentmed/facultative/services/patient/crud/PatientCRUDService.java
@@ -3,6 +3,7 @@ package pl.studentmed.facultative.services.patient.crud;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.studentmed.facultative.models.patient.Patient;
+import pl.studentmed.facultative.models.patient.PatientUpdateDTO;
 import pl.studentmed.facultative.models.user_info.UserInfo;
 
 @Service
@@ -11,6 +12,7 @@ public class PatientCRUDService {
 
     private final PatientReader reader;
     private final PatientCreator creator;
+    private final PatientUpdater updater;
 
     public Patient getPatientById(Long patientId) {
         return reader.getPatientById(patientId);
@@ -18,6 +20,15 @@ public class PatientCRUDService {
 
     public Patient createPatient(UserInfo userInfo) {
        return creator.createPatient(userInfo);
+    }
+
+    public Patient updatePatient(PatientUpdateDTO patientUpdateDTO) {
+        var patient = reader.getPatientById(patientUpdateDTO.patientId());
+        return updater.updatePatient(
+                patient,
+                patientUpdateDTO.allergies(),
+                patientUpdateDTO.medicines()
+        );
     }
 
 }
