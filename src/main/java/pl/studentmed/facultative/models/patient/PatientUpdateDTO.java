468efe856cd06157b749229bf3dd510ed12436dc
Adam <trentowski.adam@gmail.com>
2023-05-11 12:09:14 +0200

    Added PatientUpdateDTO

---
+++ src/main/java/pl/studentmed/facultative/models/patient/PatientUpdateDTO.java
@@ -0,0 +1,20 @@
+package pl.studentmed.facultative.models.patient;
+
+import pl.studentmed.facultative.models.address.AddressUpdateDTO;
+import pl.studentmed.facultative.models.user_info.UserInfoUpdateDTO;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public record PatientUpdateDTO(@NotNull
+                               Long patientId,
+                               @Valid
+                               UserInfoUpdateDTO userInfoUpdateDTO,
+                               @Size(max = 255, message = "Text size cannot exceed 255 characters.")
+                               String allergies,
+                               @Size(max = 255, message = "Text size cannot exceed 255 characters.")
+                               String medicines,
+                               AddressUpdateDTO addressUpdateDTO
+) {
+}
