83845d4dbc8bf4458fc5eae344da1f7810a4c529
Adam <trentowski.adam@gmail.com>
2023-05-15 00:26:22 +0200

    Added method to create control appointment

--- src/main/java/pl/studentmed/facultative/services/appointment/crud/AppointmentCreator.java
+++ src/main/java/pl/studentmed/facultative/services/appointment/crud/AppointmentCreator.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import pl.studentmed.facultative.models.appointment.Appointment;
 import pl.studentmed.facultative.models.appointment.AppointmentDate;
+import pl.studentmed.facultative.models.appointment.AppointmentStatus;
 import pl.studentmed.facultative.models.doctor.Doctor;
 import pl.studentmed.facultative.models.patient.Patient;
 
@@ -16,6 +17,16 @@ class AppointmentCreator {
     public Appointment createAppointment(Patient patient, Doctor doctor, AppointmentDate appointmentDate, String patientSymptoms, String medicinesTaken) {
         Appointment appointment = new Appointment(patient, doctor, appointmentDate, patientSymptoms, medicinesTaken);
         return repository.saveAndFlush(appointment);
+    }
+
+    public Appointment createControlAppointment(Appointment appointment, AppointmentDate appointmentControlDate) {
+        var patientSymptoms = "Control appointment patient's with symptoms:\n" + appointment.getPatientSymptoms();
+        Appointment controlAppointment = new Appointment(appointment.getPatient(), appointment.getDoctor(), appointmentControlDate, patientSymptoms, appointment.getMedicinesTaken());
+
+        var statusApproved = AppointmentStatus.getAppointmentStatus("approved");
+        controlAppointment.setStatus(statusApproved);
+
+        return repository.saveAndFlush(controlAppointment);
     }
 
 }
