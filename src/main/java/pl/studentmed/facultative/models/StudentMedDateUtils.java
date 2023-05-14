ab8bbb5bc682664ccb2c00aa0cac8eb187d24132
Adam <trentowski.adam@gmail.com>
2023-05-15 00:24:04 +0200

    Added list of possible appointment hours

--- src/main/java/pl/studentmed/facultative/models/StudentMedDateUtils.java
+++ src/main/java/pl/studentmed/facultative/models/StudentMedDateUtils.java
@@ -1,10 +1,14 @@
 package pl.studentmed.facultative.models;
 
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
 
 public class StudentMedDateUtils {
 
     public final static DateTimeFormatter DAY_MONTH_YEAR_TIME = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
     public final static DateTimeFormatter DAY_MONTH_YEAR = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    public final static List<String> SELECTABLE_HOURS = Arrays.asList("09:00", "09:35", "10:10", "10:45", "11:20", "12:00", "12:35",
+            "13:10", "13:45", "14:40", "15:15", "15:50", "16:25", "17:00");
 
 }
